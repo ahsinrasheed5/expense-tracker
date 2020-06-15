cce4eb06cfa6078a7a5bca66784275e2ab65623b
ahsinrasheed5 <65343061+ahsinrasheed5@users.noreply.github.com>
2020-06-15 19:21:10 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -5,17 +5,6 @@ import App from './App';
 ReactDOM.render(<App/>,document.getElementById("root"));
 
 
-
-
-
-
-
-
-
-
-
-
-
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 // import './index.css';
